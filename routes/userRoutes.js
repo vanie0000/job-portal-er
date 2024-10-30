f721f84278b78b80a5a11b2a8f21862509d0d1d7
= <=>
2024-10-30 13:27:14 +0530

    added user signup form

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,9 +8,9 @@ const {addCompany, showAll} = require('../controllers/company')
 const router = express.Router()
 
 
-router.route('/addUser').post(addUser)
+router.route('/user-signup').post(addUser)
 router.route('/allUsers').get(getAllUsers)
-router.route('/addCompany').post(addCompany)
+router.route('/company-signup').post(addCompany)
 router.route('/allCompanies').get(showAll)
 
 module.exports = router;
